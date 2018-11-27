a2dc60e2f384b62e49526cd5bcc5614de577c0ec
baptisteanorga <baptiste.anorga@yahoo.fr>
2018-11-27 23:46:11 +0100

    Fauna/flora

--- wp-content/themes/Issoucorp/inc/thumbnails.php
+++
@@ -1,5 +0,0 @@
-<?php
-add_action( 'after_setup_theme', 'thumbnails_theme_support' );
-function thumbnails_theme_support(){
-    add_theme_support( 'post-thumbnails' );
-}
--- wp-content/themes/Issoucorp/index.php
+++
@@ -1,7 +0,0 @@
-<?php get_header(); //appel du template header.php  ?>
-
-<div id="content">
-
-</div> <!-- /content -->
-
-<?php get_footer(); //appel du template footer.php ?>
